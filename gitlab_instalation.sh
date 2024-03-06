a751c139602915c169855025892415c8922748e9
vanelnara <147110104+vanelnara@users.noreply.github.com>
2024-03-06 20:12:50 +0400

    Add files via upload

--- gitlab_instalation.sh
+++ gitlab_instalation.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Install Docker
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/n>
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
@@ -29,10 +36,10 @@ services:
     hostname: "st12.sne.com"
     environment:
       GITLAB_OMNIBUS_CONFIG: |
-        external_url 'http://st12.sne.com'
+        external_url 'http://10.1.1.202'
         gitlab_rails['gitlab_shell_ssh_port'] = 22
     ports:
-      - "8080:80"
+      - "80:80"
       - "2222:22"
     volumes:
       - "/srv/gitlab/config:/etc/gitlab"
